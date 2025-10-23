867165c34be6ea7a94da2d1bcfe248dcbb018595
AntonioJRBrito <antonio.rios.brito@gmail.com>
2025-10-23 16:30:59 -0300

    Alterado o main

--- main/main.cpp
+++ main/main.cpp
@@ -7,11 +7,11 @@ extern "C" void app_main(void) {
     esp_log_level_set("*", ESP_LOG_INFO);
     ESP_LOGI("MAIN", "Teste inicial do EventBus");
     EventBus::init();
-    int counter = 1;
-    while (true) {
-        vTaskDelay(pdMS_TO_TICKS(2000));
-        char msg[32];
-        snprintf(msg, sizeof(msg), "PING %d", counter++);
-        EventBus::post(EventDomain::NETWORK,EventId::NETIF_READY,msg,strlen(msg)+1);
-    }
+    // int counter = 1;
+    // while (true) {
+    //     vTaskDelay(pdMS_TO_TICKS(2000));
+    //     char msg[32];
+    //     snprintf(msg, sizeof(msg), "PING %d", counter++);
+    //     EventBus::post(EventDomain::NETWORK,EventId::NETIF_READY,msg,strlen(msg)+1);
+    // }
 }
